949201f056c111a2ad9b03d9201f6076496ac341
Eber <www.eberacunaquispe@gmail.com>
2026-02-17 18:49:26 -0500

    Se agrupo el creado de los div en una funcion que recibe el contenedor y una cantidad asimismo se uso replaceChildren para borrar el contenedor en la funcion borrado tambien se asigno las funciones como evento para los botones

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 const container = document.querySelector("#container")
+const cambiar = document.querySelector("#btn1")
+const reiniciar = document.querySelector("#btn2")
 const array = []
 
-for(let i=0; i<256;i++){
-    const div = document.createElement("div");
-    div.classList.add("cuadricula")
-    array.push(div);
+
+function cuadricula16(container,cantidad){
+    for(let i=0; i<Math.pow(cantidad,2);i++){
+        const div = document.createElement("div");
+        div.classList.add("cuadricula")
+        div.style.height = `calc(100%/${cantidad})`
+        div.style.width = `calc(100%/${cantidad})`
+        div.style.border = "1px solid black"
+        array.push(div);
+    }
+
+    for(let i = 0; i<array.length; i++){
+        container.appendChild(array[i])
+    }
 }
 
-for(let i = 0; i<array.length; i++){
-    container.appendChild(array[i])
+function borrado(container){
+    array.length=0;
+    container.replaceChildren();
+    return true;
 }
 
+cuadricula16(container,16)
+
+cambiar.addEventListener("click",()=>{
+    let cuadro = parseInt(prompt("Ingrese un numero de cuadricula max 100:"))
+    let condicion = cuadro>100||cuadro<=0
+    if(typeof cuadro !== "number" || condicion==true){
+        alert("El valor ingresado no es valido")
+        return
+    }else{
+        let validar = borrado(container)
+        if(validar){
+            cuadricula16(container,cuadro);
+        }
+
+    }
+})
+
+reiniciar.addEventListener("click",()=>{
+    let resetear = borrado(container)
+    if(resetear){
+        cuadricula16(container,16)
+    }
+
+})
 
