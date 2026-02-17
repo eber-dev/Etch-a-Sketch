4fa0d1f53ea385dacd90f915419e93640db17c8d
Eber <www.eberacunaquispe@gmail.com>
2026-02-17 11:02:35 -0500

    Se elimino el set atribute para el estilo y se paso al css

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const array = []
 for(let i=0; i<256;i++){
     const div = document.createElement("div");
     div.classList.add("cuadricula")
-    div.setAttribute("style","border: 1px solid")
     array.push(div);
 }
 
